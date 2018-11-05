7a38ea5ab3456229c7538f60c5facc8f9f7025e0
Dan Lee <dan-lee1@hotmail.co.uk>
2018-11-05 18:41:33 +0000

    Marshland Intranet - Reborn

---
+++ beta/assets/js/scripts.js
@@ -0,0 +1,23 @@
+document.ondragstart = function () { return false; };
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML =
+    h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+
+n =  new Date();
+y = n.getFullYear();
+m = n.getMonth() + 1;
+d = n.getDate();
+document.getElementById("date").innerHTML = m + "/" + d + "/" + y;
